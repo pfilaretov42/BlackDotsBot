8c987c548936c58d4e55f82cea5d8defb8109865
Petr Filaretov <petr.filaretov@gmail.com>
2020-04-29 10:52:51 +0300

    echo photo

--- src/main/java/com/pfilaretov/bdb/bot/BlackDotsBot.java
+++ src/main/java/com/pfilaretov/bdb/bot/BlackDotsBot.java
@@ -1,9 +1,13 @@
 package com.pfilaretov.bdb.bot;
 
+import java.util.Comparator;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -27,6 +31,26 @@ public class BlackDotsBot extends TelegramLongPollingBot {
             try {
                 // Call method to send the message
                 execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // If it is a photo
+        if (update.hasMessage() && update.getMessage().hasPhoto()) {
+            // Array with photos
+            List<PhotoSize> photos = update.getMessage().getPhoto();
+
+            // Get largest photo's file_id and send it back
+            String fileId = photos.stream().max(Comparator.comparing(PhotoSize::getFileSize))
+                .orElseThrow(() -> new RuntimeException("Cannot obtain file size")).getFileId();
+            SendPhoto msg = new SendPhoto()
+                .setChatId(update.getMessage().getChatId())
+                .setPhoto(fileId)
+                .setCaption("Photo");
+            try {
+                // Call method to send the photo
+                execute(msg);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
